963e9f1c5a7f6fb744ba17c1f01c2817f1f65435
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-27 17:55:24 +0700

    Penambahan PetaNavigasi.kt

---
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,46 @@
+package com.example.myroomsatu.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+// Pastikan import destinasi dan screen sesuai package Anda
+import com.example.myroomsatu.view.HomeScreen
+import com.example.myroomsatu.view.EntrySiswaScreen
+import com.example.myroomsatu.view.route.DestinasiHome
+import com.example.myroomsatu.view.route.DestinasiEntry
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(route = DestinasiEntry.route) }
+            )
+        }
+        composable(route = DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
