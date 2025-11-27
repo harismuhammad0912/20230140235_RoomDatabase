eaafae2149ff9979b8159118e77a570b945756f8
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-27 17:52:59 +0700

    penambahan database siswa

---
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -0,0 +1,33 @@
+package com.example.myroomsatu.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Siswa::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseSiswa::class.java,
+                    "db_siswa"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
