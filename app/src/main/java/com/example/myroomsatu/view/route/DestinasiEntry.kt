c91ecf97f83cac6f6adb4883339c148ad8e9ceed
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-27 17:53:18 +0700

    penambahan DestinasiEntry.kt

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.judul_entry
+}
